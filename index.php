a78b12a2f1d733725a0832074fae9ea111738c56
jvpolak <joaovpolak@gmail.com>
2022-10-18 10:57:08 -0300

    comando delete

--- index.php
+++ index.php
@@ -24,12 +24,15 @@ require_once("config.php");
 //$aluno->insert();
 //echo $aluno;
 
+//alterar um usuario
+//$usuario = new Usuario();
+//$usuario->loadById(8);
+//$usuario->update("professor", "!@#$%¨&*");
+//echo $usuario;
+
 $usuario = new Usuario();
-
-$usuario->loadById(8);
-
-$usuario->update("professor", "!@#$%¨&*");
-
+$usuario->loadById(7);
+$usuario->delete();
 echo $usuario;
 
 ?>
